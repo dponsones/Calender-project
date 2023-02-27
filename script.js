8dba3a2bcb6c277b8b39ae70b96912ab42e75f9d
Deion Ponsones <dcponson@gmail.com>
2023-02-27 18:01:58 -0500

    added javaScript time block color

--- script.js
+++ script.js
@@ -1,16 +1,11 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-// const saveButton = $('button');
+
 
 
 $(document).ready(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
+ 
   const saveButton = document.querySelectorAll('button');
   saveButton.forEach(button => {
 
@@ -25,23 +20,26 @@ $(document).ready(function () {
     $(this).children('textarea').val(localStorage.getItem($(this).attr('id')));
   })
 
-
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //  (IF STATEMENT - .ADDCLASS? OR TOGGLECLASS PAST,PRESENT OR FUTURE)
   const currentHour = dayjs().hour();
     console.log(currentHour);
   const timeBlocks = document.querySelectorAll(".time-block");
     console.log(timeBlocks);
-  
-  
 
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
+    timeBlocks.forEach((block) => {
+      // Get the hour from the time-block's id
+      const blockHour = parseInt(block.getAttribute("id").split("-")[1]);
+    
+      if (blockHour < currentHour) {
+        // This time block is in the past
+        block.classList.add("past");
+      } else if (blockHour === currentHour) {
+        // This time block is the current hour
+        block.classList.add("present");
+      } else {
+        // This time block is in the future
+        block.classList.add("future");
+      }
+    });
   //
   // TODO: Add code to display the current date in the header of the page.
   let currentDate = dayjs();
